d92caac5424fcf2a6d34972d01948d8d8dc7e9ef
zoelabbb <zoelabbb@gmail.com>
2023-11-13 21:56:07 +0700

    modified:   go.mod

--- main.go
+++ main.go
@@ -5,13 +5,18 @@ import (
 	"go-crud-postgres/routes"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	r := router.Router()
-	// fs := http.FileServer(http.Dir("build"))
-	// http.Handle("/", fs)
-	fmt.Println("Server dijalankan pada port 8080...")
+	// Use the router from the imported package
+	r := routes.Router()
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Server is running on port %s...\n", port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
 }
--- routes/routes.go
+++ routes/routes.go
@@ -1,7 +1,7 @@
-package router
+package routes
 
 import (
-	"go-crud-postgres/controllers"
+	"go-crud-postgres/controllers" // Update the import path to match your project structure
 
 	"github.com/gorilla/mux"
 )
@@ -10,7 +10,7 @@ func Router() *mux.Router {
 
 	router := mux.NewRouter()
 
-	router.HandleFunc("/api/buku", controller.AmbilSemuaBuku).Methods("GET", "OPTIONS")
+	router.HandleFunc("/api/buku", controller.AmbilSemuaBuku).Methods("GET", "OPTIONS") // Update the controller reference
 	router.HandleFunc("/api/buku/{id}", controller.AmbilBuku).Methods("GET", "OPTIONS")
 	router.HandleFunc("/api/buku", controller.TmbhBuku).Methods("POST", "OPTIONS")
 	router.HandleFunc("/api/buku/{id}", controller.UpdateBuku).Methods("PUT", "OPTIONS")
